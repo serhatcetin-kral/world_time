e3cff9d8dab00733d9b9643eb7f89183f8ef8d2f
serhatcetin-kral <serhat06.sc@gmail.com>
2023-06-25 12:45:30 +0200

    listview builder ulkelerin liste seklinde gozukmesi

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/world_time.dart';
 
 class Choose_location extends StatefulWidget {
 
@@ -8,10 +9,19 @@ class Choose_location extends StatefulWidget {
 }
 class _Choose_locationState extends State<Choose_location> {
 
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
 
-
-  int counter=0;
+ // int counter=0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +35,39 @@ class _Choose_locationState extends State<Choose_location> {
         centerTitle: true,
         elevation: 0,
       ),
-      body:ElevatedButton(
-        onPressed: (){
-          setState(() {
-            counter+=1;
-          });
+      body:ListView.builder(
+
+        itemCount: locations.length,
+        itemBuilder: (context,index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1,horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: (){
+
+                },
+                title: Text('locations[index].location'),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/${locations[index].flag}"),
+                ),
+              ),
+            ),
+          );
+
         },
-        child: Text("counter is $counter"),
-      ) ,
+
+
+      ),
 
     );
   }
 }
+
+// ElevatedButton(
+// onPressed: (){
+// setState(() {
+// // counter+=1;
+// });
+// },
+// child: //Text("counter is $counter"),
+// )
